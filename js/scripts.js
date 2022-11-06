a57f90dab13f2f8c917c4a78abb6610a4e2d65b4
manhluong1206 <manhluong1206@gmail.com>
2022-11-06 22:19:48 +0700

    update-homepage
    3

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,6 @@
 function openNav() {
-    document.getElementById("mySidebar").style.width = "250px";
-    document.getElementById("main").style.marginLeft = "250px";
+    document.getElementById("mySidebar").style.width = "179px";
+    document.getElementById("main").style.marginLeft = "179px";
   }
   
   function closeNav() {
