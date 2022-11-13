e2553fc36c7e81ab32bb8060c7cee10b333d2599
manhluong1206 <manhluong1206@gmail.com>
2022-11-14 00:43:49 +0700

    update

--- js/scripts.js
+++ js/scripts.js
@@ -7,3 +7,19 @@ function openNav() {
     document.getElementById("mySidebar").style.width = "0";
     document.getElementById("main").style.marginLeft= "0";
 }
+
+function openContent(evt, contentName) {
+  var i, tabcontent, tablinks;
+  tabcontent = document.getElementsByClassName("tabcontent");
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
+  }
+  tablinks = document.getElementsByClassName("tablinks");
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].className = tablinks[i].className.replace(" active", "");
+  }
+  document.getElementById(contentName).style.display = "block";
+  evt.currentTarget.className += " active";
+
+}
+document.getElementById("defaultOpen").click();
